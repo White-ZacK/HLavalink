ba74376fa0e6e3aa7fd28622bab09269754878fa
White-ZacK <zakarialabiad17@gmail.com>
2020-10-01 13:43:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,10 +3,11 @@ import os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix=">")
+TOKEN = os.environ.get('TOKEN')
 
 @bot.event
 async def on_ready():
   print(f'{bot.user} has logged in.')
   bot.load_extension('cogs.WVL')
 
-bot.run("NzYwMDg2MzM0MDY0MDMzNzky.X3G7jw.Pj18QeN9jFwfgjfkkuTSx8LHlkU")
+bot.run(TOKEN)
